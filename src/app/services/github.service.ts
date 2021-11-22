724df73ec712435d75e0ea30056ee41ae0977c31
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-11-22 22:00:30 +0100

    github service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  //https://api.github.com/users/yazidelcn
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getUserDetails(userName:string){
+    return this.http.get(`https://api.github.com/users/${userName}`);
+  }
+
+  getRepos(reposUrl:string){
+    return this.http.get(reposUrl);
+  }
 }
