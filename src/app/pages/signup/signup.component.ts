884b48db8b04a24181d30400e150dd8510a80650
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-11-23 13:37:37 +0100

    Signup html template and Ts logic

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(f: NgForm){
+    const {email, password} = f.form.value;
+    this.auth.signIn(email,password)
+        .then((res)=>{
+          this.router.navigateByUrl('/');
+          this.toastr.success('Signup Successfully')
+        })
+        .catch((err)=>{
+          console.error(err.message);
+          this.toastr.error('Something went wrong !')
+        })
+  } 
 }
